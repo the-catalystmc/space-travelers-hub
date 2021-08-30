014b7ba6a7be4c29b70adca336354a675377784f
Kevin <kevinsevinche@gmail.com>
2021-08-30 18:51:23 -0500

    Create change status action for joining missions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,9 +1,15 @@
 import fetchMissions from '../../controller/missions-api';
 
 const GET_MISSIONS = 'missions/GET_MISSIONS';
+const CHANGE_STATUS = 'missions/CHANGE_STATUS';
 
 const getMissions = (payload) => ({
   type: GET_MISSIONS,
+  payload,
+});
+
+export const changeStatus = (payload) => ({
+  type: CHANGE_STATUS,
   payload,
 });
 
@@ -13,6 +19,11 @@ const missionsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_MISSIONS:
       return action.payload;
+
+    case CHANGE_STATUS:
+      return state.map((mission) => (mission.mission_id === action.payload
+        ? { ...mission, reserved: true }
+        : mission));
 
     default:
       return state;
