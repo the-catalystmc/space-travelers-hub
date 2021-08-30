b6e1d9c583341047cbebf352bd998d3e67051938
Kevin <kevinsevinche@gmail.com>
2021-08-30 17:12:41 -0500

    Display missions list

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -4,8 +4,8 @@ const Mission = ({ mission }) => (
   <tr>
     <td>{mission.mission_name}</td>
     <td>{mission.description}</td>
-    <td />
-    <td />
+    <td>NOT A MEMBER</td>
+    <td>Join Mission</td>
   </tr>
 );
 
