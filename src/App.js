c91c60377af25b81824e4be3fdb277288c5e92e0
Kevin <kevinsevinche@gmail.com>
2021-08-30 14:16:04 -0500

    Add Navlinks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import {
-  Route, BrowserRouter as Router, Switch,
-} from 'react-router-dom';
-import NavBar from './components/NavBar'
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+import NavBar from "./components/NavBar";
 // import {
 //   BrowserRouter as Router, Switch, Route, Link,
 // } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-    <div className="App">
+    <Router>
       <NavBar />
-    </div>
+      <Switch>
+        <Route path="/missions">
+          <div>miss</div>
+        </Route>
+        <Route path="/profile">
+          <div>prof</div>
+        </Route>
+        <Route path="/rockets">
+          <div>rock</div>
+        </Route>
+        <Redirect from="/" to="rockets" />
+      </Switch>
     </Router>
   );
 }
