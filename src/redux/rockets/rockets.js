8ce155500ac0efae70a2f95b9000fdd3c5edfac8
Rhyine <rhyinestewart@gmail.com>
2021-08-30 15:48:43 -0500

    Create rockets reducer

---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+
+const GET_ROCKET_REQUEST = 'GET_ROCKET_REQUEST';
+const GET_ROCKET_SUCCESS = 'GET_ROCKET_SUCCESS';
+const GET_ROCKET_FAILURE = 'GET_ROCKET_FAILURE';
+
+export const getRocketRequest = () => ({
+    type: GET_ROCKET_REQUEST,
+});
+
+export const getRocketSuccess = (rockets) => ({
+    type: GET_ROCKET_SUCCESS,
+    payload: rockets,
+});
+
+export const getRocketFailure = (error) => ({
+    type: GET_ROCKET_FAILURE,
+    payload: error,
+});
+
+const rocketsURL = "https://api.spacexdata.com/v3/rockets";
+
+export const fetchRockets = () => (dispatch) => {
+    dispatch(getRocketRequest);
+    axios.get(rocketsURL)
+        .then((response) => {
+            const rockets = response.data.forEach(rocket => ({
+                id: rocket.id,
+                rocket_name: rocket.name,
+                description: rocket.description,
+                flickr_images: rocket.flickr_images[0],
+            }));
+            dispatch(getRocketSuccess(rockets));
+        })
+        .catch((error) => {
+            const errorMsg = error.message;
+            dispatch(getRocketFailure(errorMsg));
+        })
+}
+
+export const initialState = {
+    loading: false,
+    rockets: [],
+    error: '',
+}
+
+const rocketsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_ROCKET_REQUEST:
+            return { ...state, loading: true };
+
+        case GET_ROCKET_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                rockets: [...state.rockets, action.payload],
+                error: '',
+            };
+
+        case GET_ROCKET_FAILURE:
+            return {
+                loading: false,
+                rockets: [],
+                error: action.payload,
+            }
+
+        default:
+            return state;
+    }
+}
+
+export default rocketsReducer;
