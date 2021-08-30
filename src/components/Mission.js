7cb820e42dc306836a6bf48cdbbb3aef0d6a219e
Kevin <kevinsevinche@gmail.com>
2021-08-30 17:42:52 -0500

    Add join button to missions table

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -4,8 +4,14 @@ const Mission = ({ mission }) => (
   <tr>
     <td>{mission.mission_name}</td>
     <td>{mission.description}</td>
-    <td>NOT A MEMBER</td>
-    <td>Join Mission</td>
+    <td>
+      <span className="not-member">NOT A MEMBER</span>
+    </td>
+    <td>
+      <button type="button" className="join-mission">
+        Join Mission
+      </button>
+    </td>
   </tr>
 );
 
