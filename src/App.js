aae2a1e85abac7d7600e38ea4773501bd1b50d0f
Kevin <kevinsevinche@gmail.com>
2021-08-30 16:33:33 -0500

    Use useEffect to load the missions once

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { Redirect } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import { getMissionsThunk } from './redux/missions/missions';
+import NavBar from './components/NavBar';
+import Missions from './components/Missions';
 // import {
 //   BrowserRouter as Router, Switch, Route, Link,
 // } from 'react-router-dom';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMissionsThunk());
+  }, []);
+
   return (
     <Router>
       <NavBar />
       <Switch>
         <Route path="/missions">
-          <div>miss</div>
+          <Missions />
         </Route>
         <Route path="/profile">
           <div>prof</div>
