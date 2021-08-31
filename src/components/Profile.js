439b2e5d68b57a91ccea16a520549afae9f3504c
Rhyine <rhyinestewart@gmail.com>
2021-08-31 15:14:55 -0500

    Add reserved rockets to my profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,11 @@ const Profile = () => {
   const missions = useSelector((state) => state.missionsReducer);
   const filteredMissions = missions.filter((mission) => mission.reserved);
 
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const filteredRockets = rockets.filter((rocket) => rocket.reserved);
+
+  console.log(filteredRockets);
+
   return (
     <div className="profile-wrapper">
       <div className="my-missions">
@@ -19,11 +24,19 @@ const Profile = () => {
       </div>
       <div className="my-rockets">
         <h2>My Rockets</h2>
+        {filteredRockets.length !== 0 && (
+          <ul>
+            {filteredRockets.map((rocket) => (
+              <MyRockets key={rocket.id} rocket={rocket} />
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
 };
 
 const MyMissions = ({ mission }) => <li>{mission.mission_name}</li>;
+const MyRockets = ({ rocket }) => <li>{rocket.rocketName}</li>;
 
 export default Profile;
