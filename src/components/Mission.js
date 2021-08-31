e4f1d6474b07d98154867bfa465fde287b0fe176
Kevin <kevinsevinche@gmail.com>
2021-08-31 11:20:32 -0500

    Add React conditional rendering

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React from "react";
 
 const Mission = ({ mission, joinMission }) => (
   <tr>
     <td>{mission.mission_name}</td>
     <td>{mission.description}</td>
-    <td>
-      <span className="not-member">NOT A MEMBER</span>
-    </td>
-    <td>
-      <button
-        value={mission.mission_id}
-        type="button"
-        className="join-mission"
-        onClick={joinMission}
-      >
-        Join Mission
-      </button>
-    </td>
+    {mission.reserved && (
+      <td>
+        <span className="active-member">Active Member</span>
+      </td>
+    )}
+    {!mission.reserved && (
+      <td>
+        <span className="not-member">NOT A MEMBER</span>
+      </td>
+    )}
+    {mission.reserved && (
+      <td>
+        <button
+          value={mission.mission_id}
+          type="button"
+          className="leave-mission"
+          onClick={joinMission}
+        >
+          Leave Mission
+        </button>
+      </td>
+    )}
+    {!mission.reserved && (
+      <td>
+        <button
+          value={mission.mission_id}
+          type="button"
+          className="join-mission"
+          onClick={joinMission}
+        >
+          Join Mission
+        </button>
+      </td>
+    )}
   </tr>
 );
 
