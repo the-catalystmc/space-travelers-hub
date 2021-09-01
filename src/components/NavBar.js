f51f415347bc63209425a0f674a829db919f6c7f
Rhyine <rhyinestewart@gmail.com>
2021-09-01 12:21:08 -0500

    Update Routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import logo from '../assets/planet.png';
 const Navbar = () => (
   <div className="Nav-Container">
     <div className="Header-Logo">
-      <NavLink exact to="/">
+      <NavLink to="/">
         <img src={logo} alt="Logo" />
       </NavLink>
       <h1 className="Header-Title">Space Travelers Hub</h1>
