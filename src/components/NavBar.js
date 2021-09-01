ab11ea32f79347b6faae0ace42c04abba2188305
Kevin <kevinsevinche@gmail.com>
2021-09-01 15:01:10 -0500

    Update logo anchor

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,34 @@
-import { NavLink } from 'react-router-dom';
-import '../scss/style.scss';
-import logo from '../assets/planet.png';
+import { NavLink, Link } from "react-router-dom";
+import "../scss/style.scss";
+import logo from "../assets/planet.png";
 
 const Navbar = () => (
   <div className="Nav-Container">
     <div className="Header-Logo">
-      <NavLink to="/rockets">
+      <Link to="/rockets">
         <img src={logo} alt="Logo" />
-      </NavLink>
-      <h1 className="Header-Title">Space Travelers Hub</h1>
+        <h1 className="Header-Title">Space Travelers Hub</h1>
+      </Link>
     </div>
     <nav>
       <ul className="Nav-Items">
         <li className="Nav-Item">
-          <NavLink to="/rockets">Rockets</NavLink>
+          <NavLink className="a-nav" to="/rockets">
+            Rockets
+          </NavLink>
         </li>
         <li className="Nav-Item">
-          <NavLink to="/missions">Missions</NavLink>
+          <NavLink className="a-nav" to="/missions">
+            Missions
+          </NavLink>
         </li>
         <li className="divider Nav-Item">
           <p>|</p>
         </li>
         <li className="Nav-Item">
-          <NavLink to="/profile">My Profile</NavLink>
+          <NavLink className="a-nav" to="/profile">
+            My Profile
+          </NavLink>
         </li>
       </ul>
     </nav>
